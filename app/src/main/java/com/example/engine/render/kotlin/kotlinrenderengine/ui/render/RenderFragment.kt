cea3e282fdd12bf12c52df3912f3ca71d1f5113a
Seph <me@joepalmer.co.uk>
2024-06-22 09:35:20 +0100

    Update project
    Fix compatibility issue with Android Studio

--- app/src/main/java/com/example/engine/render/kotlin/kotlinrenderengine/ui/render/RenderFragment.kt
+++ app/src/main/java/com/example/engine/render/kotlin/kotlinrenderengine/ui/render/RenderFragment.kt
@@ -1,14 +1,15 @@
 package com.example.engine.render.kotlin.kotlinrenderengine.ui.render
 
 import android.arch.lifecycle.ViewModelProviders
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.Surface
 import android.view.View
 import android.view.ViewGroup
 import com.example.engine.render.kotlin.kotlinrenderengine.R
 import com.example.engine.render.kotlin.kotlinrenderengine.engine.BasicRenderer
-import kotlinx.android.synthetic.main.render_fragment.*
 
 class RenderFragment : Fragment() {
 
@@ -26,10 +27,11 @@ class RenderFragment : Fragment() {
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        val surface = activity?.findViewById<GLSurfaceView>(R.id.surface)
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(RenderViewModel::class.java)
-        surface.setEGLContextClientVersion(2)
-        surface.setRenderer(context?.let { BasicRenderer() })
+        surface?.setEGLContextClientVersion(2)
+        surface?.setRenderer(context?.let { BasicRenderer() })
     }
 
 }
